0b84c3e817d5d225d5ba181d20a7b4c20932247f
Beam <joshuajbarnett1@gmail.com>
2026-04-19 15:36:44 -0700

    dummy routes page to test per PR deployment

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,6 +1,7 @@
-import { useAuth0 } from '@auth0/auth0-react';
-import { useSyncUserToBackend } from './hooks/useSyncUserToBackend';
-import './App.css'
+import { useAuth0 } from "@auth0/auth0-react";
+import { useSyncUserToBackend } from "./hooks/useSyncUserToBackend";
+import "./App.css";
+import { RoutesPage } from "./routesPage";
 
 function App() {
   const {
@@ -16,32 +17,37 @@ function App() {
 
   const handleSignup = () => {
     login({ authorizationParams: { screen_hint: "signup" } });
-  }
-    
+  };
+
   const handleLogout = () => {
     auth0Logout({ logoutParams: { returnTo: window.location.origin } });
-  }
+  };
 
   const handleLogin = () => {
     login();
-  }
+  };
 
   if (isLoading) return "Loading...";
 
-  return isAuthenticated ? (
+  return (
     <>
-      <p>Logged in as {user?.email}</p>
-      <h1>User Profile</h1>
-      <pre>{JSON.stringify(user, null, 2)}</pre>
-      <button onClick={handleLogout}>Logout</button>
-    </>
-  ) : (
-    <>
-      {error && <p>Error: {error.message}</p>}
-      <button onClick={handleSignup}>Signup</button>
-      <button onClick={handleLogin}>Login</button>
+      {isAuthenticated ? (
+        <>
+          <p>Logged in as {user?.email}</p>
+          <h1>User Profile</h1>
+          <pre>{JSON.stringify(user, null, 2)}</pre>
+          <button onClick={handleLogout}>Logout</button>
+        </>
+      ) : (
+        <>
+          {error && <p>Error: {error.message}</p>}
+          <button onClick={handleSignup}>Signup</button>
+          <button onClick={handleLogin}>Login</button>
+        </>
+      )}
+      <RoutesPage />
     </>
   );
 }
 
-export default App
+export default App;
---
+++ frontend/src/routesPage.tsx
@@ -0,0 +1,8 @@
+export const RoutesPage = () => {
+  return (
+    <div>
+      <h1>Routes Page</h1>
+      <p>This is the routes page.</p>
+    </div>
+  );
+};
